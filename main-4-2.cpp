5021be1ecf27d90cb28a19335a37a496dd62448b
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-10-24 14:23:19 +1030

    cheer

---
+++ main-4-2.cpp
@@ -0,0 +1,17 @@
+#include "sort_by_animalID.h"
+#include "sort_by_name.h"
+#include "zoo.h"
+#include "animal.h"
+#include "hunter.h"
+#include "vegie.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
---
+++ sort_by_name.h
@@ -0,0 +1,32 @@
+#ifndef SORT_BY_NAME_H
+#define SORT_BY_NAME_H
+#include "zoo.h"
+#include "animal.h"
+#include "hunter.h"
+#include "vegie.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class sort_by_Name{
+    public:
+        static void sort(animal **animals,int n)
+        {
+           for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n-i-1;j++)
+                {
+                    if(animals[j]->get_name() > animals[j+1]->get_name())
+                    {
+                        animal *temp = animals[j];
+                        animals[j] = animals[j+1];
+                        animals[j+1] = temp;
+                    }
+                }
+            }          
+        }
+
+};
+
+#endif
