1ce80bfb751df88f3a31cc72db29f404e22f614c
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-10-24 14:17:37 +1030

    cheer

--- sort_by_animalID.cpp
+++ sort_by_animalID.cpp
@@ -8,20 +8,3 @@
 #include <stdio.h>
 
 using namespace std;
-
-static void sort_by_animalID::sort(animal **animals,int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n-i-1;j++)
-        {
-            if(animals[j] > animals[j+1])
-            {
-                animal temp = animals[j];
-                animals[j] = animals[j+1];
-                animals[j+1] = temp;
-
-            }
-        }
-    }
-}
